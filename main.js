2746cd0999bb0da522f81358583980b2fabd3183
Anna Kusmierek <aniak1100@gmail.com>
2019-10-16 19:33:19 +0100

    Add coments to code

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+// Variables
 const btn = document.querySelector(".info");
 const bar = document.querySelector(".frame .progress");
 
+// Functions:
+// Reset function
+const reset = () => {};
+
+// Stretching spring
 const stretchSpring = () => {
   btn.innerText = "release the spring";
   bar.style.width = "50%";
 };
+
+// Releasing spring
 const releaseSpring = () => {
   btn.innerText = "impact power is...";
 };
 
+// Events
 btn.addEventListener("mousedown", stretchSpring);
 btn.addEventListener("mouseup", releaseSpring);
